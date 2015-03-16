47295be6009bd077d6bcc535bb57868e46c3f88b
ddsnowboard <ddsnowboard@gmail.com>
2015-03-15 21:01:12 -0500

    Rename, add timeout, fix formatting, add debug function
    Also make caps not count in markov chain,

--- namegen.py
+++ namegen.py
@@ -9,6 +9,7 @@
 import random
 import sys
 from collections import Counter, defaultdict
+from time import time
 if sys.version_info[0] == 3:
     raw_input = input
     xrange = range
@@ -26,7 +27,7 @@ class MarkovState:
             return None
         if len(self.transitions) == 1:
             return list(self.transitions.values())[0]
-        count = sum([self.transitions[key] for key in self.transitions]) * 1.0
+        count = float(sum([self.transitions[key] for key in self.transitions]))
         r = random.uniform(0, count)
         t = 0
         for s in self.transitions:
@@ -40,30 +41,45 @@ class MarkovChain:
         self.states = defaultdict(MarkovState) #allows unknown keys to default to a blank state; simplifies linking.
         self.states[haltstate] = None #Why no state? We need to get an error if we try to transition from the halt state.
         self.init = MarkovState()
+        self.breaking = False
         self.between = between
 
-    def RandomWalk(self, maxlength=0):
+    def DEBUG_printStates(self):
+        for i, j in self.states.items():
+            print(i)
+            for l, k in j.transitions.items():
+                print("   {}, {}".format(l, k))
+                
+    def randomWalk(self, maxlength=None):
+        if self.breaking:
+            return ""
         state = self.init.transition()
         output = []
+        t_0 = time()
         while state != self.haltstate:
+            if time()-t_0 > 10:
+                print("That's all I could find")
+                self.breaking = True
+                return ""
             output.append(state)
-            if maxlength > 0 and len(output) >= maxlength:
+            if maxlength and len(output) >= maxlength:
                 break
             nextstate = self.states[state].transition()
             state = nextstate
-        return self.between.join(output)
+        return self.between.join((str(i) for i in output))
 
-    def AddLink(self, linkstate, targetstate):
+    def addLink(self, linkstate, targetstate):
         self.states[linkstate].increment(targetstate)
 
-    def AddWord(self, word, terminate=False):  #Adds each letter pair in the word to the markov chain
+    def addWord(self, word, terminate=False):  #Adds each letter pair in the word to the markov chain
+        word = word.lower()
         self.init.increment(word[0])
         for n in xrange(0, len(word)-1):
-            self.AddLink(word[n], word[n+1])
+            self.addLink(word[n], word[n+1])
         if terminate:
-            self.AddLink(word[-1], self.haltstate)
+            self.addLink(word[-1], self.haltstate)
 
-    def AddWords(sentence): #Adds each word pair in the sentence to the markov chain
+    def addWords(sentence): #Adds each word pair in the sentence to the markov chain
         pass
         
 if __name__ == '__main__':
@@ -74,18 +90,17 @@ if __name__ == '__main__':
         with open(sys.argv[1], "r") as f:
             for line in f:
                 wordcount += 1
-                mc.AddWord(line)
+                mc.addWord(line)
     else:
         print("Enter words, provide blank input to end:")
-        txt = '_'
-        while txt != '\n':
+        while True:
             txt = raw_input() + '\n'
             wordcount += 1
-            mc.AddWord(txt)
-
-    t_total = sum([len(mc.states[s].transitions) for s in mc.states if not mc.states[s] is None])
+            mc.addWord(txt)
+            if txt == "\n":
+                break
+    t_total = sum([len(mc.states[s].transitions) for s in mc.states if mc.states[s]])
     print("States: {}\n\nTotal Transitions: {}\nAverage Transitions per state: {}"
         .format(len(mc.states), t_total, (t_total*1.0)/len(mc.states)))
     for n in xrange(20):
-        print(mc.RandomWalk().strip('\n'))
-
+        print(mc.randomWalk().strip('\n'))
